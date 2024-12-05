5d53ad7225f3d989fdb27c7339cf0ce53966536c
The_Tedstar <edward.aplin@st-annes.ox.ac.uk>
2024-12-05 15:23:15 +0000

    deleting test files

--- Functions/Functions for Quarto Assignment.R
+++ Functions/Functions for Quarto Assignment.R
@@ -1,16 +1,32 @@
 #Boxplot creation
 BoxGraph <- function(Dataset, Yaxis, Ytitle, Xaxis, Xtitle, 
                      Title, Stat){
+  Summary <- Dataset %>%
+    group_by(Year) %>%
+    summarise(
+      Mean = mean(Depth),
+      Lower = mean(Depth) - qt(0.975, df = n() - 1) * sd(Depth) / sqrt(n()),
+      Upper = mean(Depth) + qt(0.975, df = n() - 1) * sd(Depth) / sqrt(n())
+    )
+  
   plot <- ggplot(Dataset, aes(y = Yaxis, x = Xaxis, 
                               colour = Xaxis)) +
     theme_bw() +
     (if (Stat != TRUE) {
-      geom_boxplot(width = 0.7)
+      geom_boxplot(width = 0.6)
     }) +
     geom_beeswarm(dodge.width = 1.5, alpha = 0.5) +
     (if (Stat == TRUE) {
-      geom_smooth(method = "lm", aes(group = 1), se = FALSE, 
-                  linetype = "solid", colour = "black")
+      geom_smooth(method = "lm", aes(group = 1), 
+                  se = FALSE, linetype = "solid", colour = "#606060")
+    }) +
+    (if (Stat == TRUE) {
+      geom_errorbar(data = Summary, aes(x = Year, 
+                                        ymin = Lower, ymax = Upper),
+                    width = 0.08, size = 0.6, color = "#606060", inherit.aes = FALSE) 
+    }) +
+    (if (Stat == TRUE) {
+      stat_summary(fun = "mean", geom = "point", size = 4, color = "black", shape = 18) 
     }) +
     theme(axis.text.y   = element_text(size = 14, color = "black"),
           axis.text.x   = element_text(size = 14, color = "black", 
